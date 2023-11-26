e588dbb1ed603b0b214ff3cf0f0b4318d3a13859
KanishkRajMittal <152096720+KanishkRajMittal@users.noreply.github.com>
2023-11-27 04:11:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,5 +14,16 @@ def execute_script(gmail):
     except Exception as e:
         return jsonify({'success': False, 'error': str(e)})
 
+@app.route('/update>', methods=['POST','GET'])
+def execute_script():
+    try:
+        # Run your Python script using subprocess
+        result = subprocess.run(['python3', 'update.py'], capture_output=True, text=True)
+        output = result.stdout
+        return jsonify({'success': True, 'output': output})
+    except Exception as e:
+        return jsonify({'success': False, 'error': str(e)})
+
+
 if __name__ == '__main__':
     app.run(debug=True)
