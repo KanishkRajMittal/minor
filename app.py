b32782321eeb05c186120cdfee1f9af409852a8b
KanishkRajMittal <152096720+KanishkRajMittal@users.noreply.github.com>
2023-11-27 03:57:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def execute_script(gmail):
     try:
         # Run your Python script using subprocess
-        result = subprocess.run(['python3', '/home/kanishk/Desktop/upload/prof_maker.py',gmail], capture_output=True, text=True)
+        result = subprocess.run(['python3', 'prof_maker.py',gmail], capture_output=True, text=True)
         output = result.stdout
         return jsonify({'success': True, 'output': output})
     except Exception as e:
