1c653d26ae5a9525bcf8a6bfa8b2607d2bd77afd
KanishkRajMittal <152096720+KanishkRajMittal@users.noreply.github.com>
2023-11-27 21:52:48 +0530

    Update update.py

--- update.py
+++ update.py
@@ -5,6 +5,28 @@ from bs4 import BeautifulSoup
 import requests
 import datetime
 import pytz
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver import Chrome
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+from datetime import date
+from bs4 import BeautifulSoup
+import requests
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+
+count=1
+
+def keep_numeric_digits(input_string):
+    return ''.join(char for char in input_string if char.isdigit())
+
+
 
 def leetcode(handle):
     d={}
@@ -82,10 +104,19 @@ def codeforces(handle):
 
     d["Number_Of_Contributions"]=contributions
     d["Current_Rating"]=rating
-    d["Number_Of_Friends"]:No_of_friends
+    d["Number_Of_Friends"]=No_of_friends
     d["Current_Rank"]=Rank
     d["Max_Rating"]=max_rating
     d["Max_Rank"]=max_rank
+
+    url="https://codeforces.com/profile/"+handle
+    response=requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    problem_solved=soup.find_all(class_='_UserActivityFrame_counterValue')
+    No_of_ques_solve=problem_solved[0].text
+    max_streak=problem_solved[3].text
+    d["Number_Of_Ques_solved"] = keep_numeric_digits(No_of_ques_solve)
+    d["Max_Streak"]=max_streak
     return d
 
 
@@ -100,6 +131,18 @@ def gfg(handle):
     Coding_score=score_card[0].text
     problem_solved=score_card[1].text
     monthly_coding_score=score_card[2].text
+
+    # Find the tabs containing the number of questions for each difficulty
+    tabs = soup.find_all('li', class_='tab')
+    # Create a dictionary to store the count for each difficulty
+    difficulty_count = {'easy': 0, 'medium': 0, 'hard': 0}
+    for tab in tabs:
+        # Extract the difficulty level and count from the 'href' attribute
+        difficulty = tab.find('a')['href'].replace('#', '')
+        count = int(tab.find('a').text.split('(')[1].split(')')[0])
+        # Update the dictionary with the count for the corresponding difficulty
+        difficulty_count[difficulty] = count
+    
     d={}
     d["Handle"]=handle
     # d["Rank"]=rank
@@ -108,6 +151,10 @@ def gfg(handle):
     d["Coding_Score"]=Coding_score
     d["Problem_Solved"]=problem_solved
     d["Monthly_Coding_Score"]=monthly_coding_score
+    d["Easy_Ques_Solved"]=difficulty_count["easy"]
+    d["Medium_Ques_Solved"]= difficulty_count['medium']
+    d["Hard_Ques_Solved"] = difficulty_count['hard']
+
     return d
 
 
@@ -138,8 +185,117 @@ def get_upcoming_day_date(day_name,time):
 
     
 
-def contest_update():
-    #CodeForces
+
+def extract_info(problem_description):
+    # Find the indices of key phrases
+    problem_statement_index = problem_description.find("Problem Statement")
+    examples_index = problem_description.find("Example")
+    constraints_index = problem_description.find("Constraints")
+    your_task_index = problem_description.find("Your Task")
+
+    # Extract the corresponding sections using string slicing
+    problem_statement = problem_description[problem_statement_index + len("Problem Statement"):examples_index].strip()
+    examples_section = problem_description[examples_index + len("Examples"):constraints_index].strip()
+    your_task = problem_description[your_task_index + len("Your Task"):constraints_index].strip()
+    constraints = problem_description[constraints_index + len("Constraints"):].strip()
+
+    # Convert examples_section to a dictionary
+    # examples_dict = {}
+    # examples_list = examples_section.split("\n")
+    # current_example = ""
+    # for line in examples_list:
+    #     line = line.strip()
+    #     if line.startswith("Example"):
+    #         current_example = line
+    #         examples_dict[current_example] = {"Input": {}, "Output": "", "Explanation": ""}
+    #     elif line.startswith("Input:"):
+    #         examples_dict[current_example]["Input"] = eval(line[len("Input:"):].strip())
+    #     elif line.startswith("Output:"):
+    #         examples_dict[current_example]["Output"] = eval(line[len("Output:"):].strip())
+    #     elif line.startswith("Explanation:"):
+    #         examples_dict[current_example]["Explanation"] = line[len("Explanation:"):].strip()
+
+    # Create the final dictionary
+
+    problem_statement=problem_statement.replace("\n","@!")
+    examples_section=examples_section.replace("\n","@!")
+    constraints=constraints.replace("\n","@!")
+    your_task=your_task.replace("\n","@!")
+    problem_dict = {
+        "Problem Statement": problem_statement,
+        "Examples": examples_section,
+        "Constraints": constraints+"@! Your Task: "+your_task
+        # "Your Task": your_task
+    }
+    return problem_dict
+
+
+
+def leetcode_potd():
+    driver = webdriver.Chrome()
+    driver.get("https://leetcode.com/problemset/all/")
+    sleep(3)
+
+    # Click on the "Solve problem" button
+    daily_ques = driver.find_element(By.XPATH, '(//div[@class="truncate"])[1]')
+    daily_ques.click()
+
+    sleep(2)
+    driver.switch_to.window(driver.window_handles[0])
+    # print(driver.current_url)
+
+    title = driver.find_element(By.XPATH, "//div[@class='flex h-full items-center']").text
+    ques = driver.find_element(By.XPATH, '(//p)[1]').text+driver.find_element(By.XPATH, '(//p)[2]').text
+    ex1 = driver.find_element(By.XPATH, '(//pre)[1]').text
+    ex2 = driver.find_element(By.XPATH, '(//pre)[2]').text
+    constraints = driver.find_element(By.XPATH, '(//ul)[2]').text
+
+    title=title.replace("\n","@!")
+    ques=ques.replace("\n","@!")
+    constraints= constraints.replace("\n","@!")
+    ex1=ex1.replace("\n","@!")
+    ex2=ex2.replace("\n","@!")
+
+    data = {"LeetCode": {"title":title,"Problem Statement": ques, "Examples": ex1+"@!"+ex2,
+                    "Constraints": constraints}}
+    # print(data["LeetCode"]["Examples"])
+    return data
+
+
+def gfg_potd():
+    # Create a webdriver instance
+    # Create a webdriver instance
+    driver = Chrome()
+
+    # Navigate to the link
+    driver.get("https://www.geeksforgeeks.org/problem-of-the-day")
+    sleep(5)
+
+    # Click on the "Solve problem" button using JavaScript
+    solve_button = driver.find_element(
+        By.XPATH, '//button[@class="ui button problemOfTheDay_POTDCntBtn__SSQfX"]')
+    driver.execute_script("arguments[0].click();", solve_button)
+    sleep(1)
+    driver.switch_to.window(driver.window_handles[1])
+    s = driver.current_url
+
+
+    attributes = driver.find_element(
+        By.XPATH, '(//div)[@class="problems_problem_content__Xm_eO"]').text
+    # print(attributes)
+
+    # Close the browser
+    driver.quit()
+    # Extract information and print the dictionary
+    attributes="Problem Statement"+attributes
+    result_dict = extract_info(attributes)
+    d={}
+    d["GeeksForGeeks"]=result_dict
+    return d
+
+
+
+def codeforces_ques():
     url=" https://codeforces.com/api/contest.list?gym=false"
     response=requests.get(url)
 
@@ -147,6 +303,17 @@ def contest_update():
         raise print('User not Found')
     d={}
     profile=response.json()["result"]
+
+
+def codeforces_contest():
+    url=" https://codeforces.com/api/contest.list?gym=false"
+    response=requests.get(url)
+
+    if response.json()["status"] != "OK":
+        raise print('User not Found')
+    d={}
+    profile=response.json()["result"]
+    global count
     for contest in profile[0:5]:
         # d["c_id"]=contest["id"]
         c_name=contest["name"]
@@ -158,37 +325,58 @@ def contest_update():
         formatted_start_time = start_time_ist.strftime('%Y-%m-%d %H:%M:%S')
         date=str(start_time_ist.date())
         time=str(start_time_ist.time())
-        d[c_name]={"c_date":date,"c_time":time}
-    db.collection("contest").document("codeforces").set(d)
+        d[str(count)]={"Name":c_name,"Platform":"Codeforces","Date":date,"Time":time}
+        count=count+1
+    return d
+    # db.collection("contests").document("contests").set(d)
 
-    #leetcode
+def leetcode_contest():
     url="https://leetcode.com/contest/"
     response=requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
     d={}
+    global count
     name=soup.find_all(class_='transition-colors group-hover:text-blue-s dark:group-hover:text-dark-blue-s')
     c_name=name[0].text
     c_date=str(get_upcoming_day_date("Sunday","08:00:00"))
     c_time="08:00:00"
-    d[c_name]={"c_date":c_date,"c_time":c_time}
+    d[str(count)]={"Name":c_name,"Platform":"Leetcode","Date":c_date,"Time":c_time}
+    count=count+1
+
+
 
     c_name=name[1].text
     c_date=str(get_upcoming_day_date("Saturday","20:00:00"))
     c_time="20:00:00"
-    d[c_name]={"c_date":c_date,"c_time":c_time}
-    db.collection("contest").document("leetcode").set(d)
+    d[str(count)]={"Name":c_name,"Platform":"Leetcode","Date":c_date,"Time":c_time}
+    count=count+1
+    return d
+    # db.collection("contests").document("contests").set(d)
 
-
-    #gfg
+def gfg_contest():
     url="https://practice.geeksforgeeks.org/events/rec/gfg-weekly-coding-contest"
     response=requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
     d={}
+    global count
     c_name=soup.find(class_='sofia-pro events_upcomingEventDescTxt__xgvgK').text
     c_date=str(get_upcoming_day_date("Sunday","19:00:00"))
     c_time="19:00:00"
-    d[c_name]={"c_date":c_date,"c_time":c_time}
-    db.collection("contest").document("gfg").set(d)
+    d[str(count)]={"Name":c_name,"Platform":"GeeksForGeeks","Date":c_date,"Time":c_time}
+    count=count+1
+    return d
+    # db.collection("contests").document("contests").set(d)
+    # d[c_name]={"c_date":c_date,"c_time":c_time}
+    # db.collection("contest").document("gfg").set(d)
+
+def contest_update():
+    dict1 = leetcode_contest()
+    dict2 = codeforces_contest()
+    dict3 = gfg_contest()
+
+    # Merge dictionaries using {**d1, **d2, **d3} syntax
+    d = {**dict1, **dict2, **dict3}
+    db.collection("contests").document("contests").set(d)
 
 def user_update():   
     users = db.collection("users").get()
@@ -209,9 +397,20 @@ def user_update():
 
 
 
+def POTD_update():
+    dict1 = leetcode_potd()
+    # dict2 = codeforces_ques()
+    dict3 = gfg_potd()
+
+    # Merge dictionaries using {**d1, **d2, **d3} syntax
+    d = {**dict1,**dict3}
+    db.collection("questions").document("POTD").set(d)
+
+
 cred = credentials.Certificate("credentials.json")
 firebase_admin.initialize_app(cred)
 db=firestore.client() 
 user_update()
 contest_update()
+POTD_update()
 
